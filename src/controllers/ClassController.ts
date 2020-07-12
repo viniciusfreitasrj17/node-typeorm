a75005ca57181e345f6dc83b9922703dc46ce249
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-07-12 14:51:20 -0300

    💪 Feeding ClassController, adds index, show and store

--- src/controllers/ClassController.ts
+++ src/controllers/ClassController.ts
@@ -3,15 +3,44 @@ import { getRepository } from 'typeorm';
 import Class from '../models/Class';
 
 class ClassController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    try {
+      const repo = getRepository(Class);
+      const data = await repo.find();
+
+      return res.status(200).json(data);
+    } catch (err) {
+      // console.log(err.message);
+      return res.status(400).json({ Mensagge: 'Index Class Failed' });
+    }
+  }
+
+  public async show(req: Request, res: Response): Promise<Response> {
+    const { name } = req.params;
+    try {
+      const repo = getRepository(Class);
+      const data = await repo.find({
+        where: {
+          name
+        }
+      });
+
+      return res.status(200).json(data);
+    } catch (err) {
+      // console.log(err.message);
+      return res.status(400).json({ Mensagge: 'Show Class Failed' });
+    }
+  }
+
   public async store(req: Request, res: Response): Promise<Response> {
     try {
       const repo = getRepository(Class);
-      const response = await repo.save(req.body);
+      const data = await repo.save(req.body);
 
-      return res.status(200).json(response);
+      return res.status(200).json(data);
     } catch (err) {
-      console.log(err.message);
-      return res.status(400).json({ Mensagge: 'Register Failed' });
+      // console.log(err.message);
+      return res.status(400).json({ Mensagge: 'Store Class Failed' });
     }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import ClassController from './controllers/ClassController';
 
 const routes = Router();
 
+routes.get('/class', ClassController.index);
+routes.get('/class/:name', ClassController.show);
 routes.post('/class', ClassController.store);
 
 export default routes;
